3c364f5c470582bd362f2cf0ff679547328a66d7
rmgimenez <rmgimenez@gmail.com>
2016-10-10 07:56:22 -0300

    alterações

--- src/livro/MiniMusicPlayer2Pg277.java
+++ src/livro/MiniMusicPlayer2Pg277.java
@@ -1,14 +1,32 @@
 package livro;
 
+import javax.sound.midi.MidiEvent;
+import javax.sound.midi.ShortMessage;
+
 public class MiniMusicPlayer2Pg277 {
 
 	public static void main(String[] args) {
-		
+
+	}
+
+	public void go() {
 
 	}
 	
-	public void go(){
-		
+	public void controlChange(ShortMessage event){
+		System.out.println("la");
+	}
+
+	public static MidiEvent makeEvent(int comd, int chan, int one, int two, int tick) {
+		MidiEvent event = null;
+		try {
+			ShortMessage a = new ShortMessage();
+			a.setMessage(comd, chan, one, two);
+			event = new MidiEvent(a, tick);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return event;
 	}
 
 }
