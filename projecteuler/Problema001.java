318b3090940f497a318c4f3d908771dca02d2b9d
rmgimenez <rmgimenez@gmail.com>
2016-10-30 19:28:34 -0200

    ALteração

--- projecteuler/Problema001.java
+++ projecteuler/Problema001.java
@@ -12,14 +12,15 @@ public class Problema001
 	public static void main(String[] args)
 	{
 		int soma = 0;
-		
-		for(int i = 0; i < 1000; i++)
+
+		for (int i = 0; i < 1000; i++)
 		{
-			if(((i % 3) == 0) || ((i % 5) == 0)){
+			if (((i % 3) == 0) || ((i % 5) == 0))
+			{
 				soma = soma + i;
 			}
 		}
-		
+
 		System.out.println(soma);
 	}
 
